a3470835533b126c305e4765edb879b2669aec7d
RiX <rix1337@users.noreply.github.com>
2019-10-25 20:18:37 +0200

    Clutter at /tmp cleaned
    Thanks @shidraconis

--- root/ripper/ripper.sh
+++ root/ripper/ripper.sh
@@ -25,7 +25,10 @@ let BAD_RESPONSE=0
 while true
 do
 # delete MakeMKV temp files
-find /tmp/ -name "*.tmp" -type f -delete
+cwd=$(pwd)
+cd /tmp
+rm -r *.tmp
+cd $
 
 # get disk info through makemkv and pass output to INFO
 INFO=$"`makemkvcon -r --cache=1 info disc:9999 | grep DRV:0`"
