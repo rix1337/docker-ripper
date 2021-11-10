28970af155dff3195f3a5effc23ac5dcf9b6dcef
RiX <rix1337@users.noreply.github.com>
2021-11-10 11:25:33 +0100

    Update web.sh

--- manual-build/root/etc/my_init.d/web.sh
+++ manual-build/root/etc/my_init.d/web.sh
@@ -3,4 +3,4 @@
 echo "Starting web ui"
 
 chmod +x /web/web.py
-python3 /web/web.py --port=9090 --prefix=$PREFIX --log=/config/Ripper.log --user=$USER --pass=$PASS
+python3 /web/web.py --port=9090 --prefix=$OPTIONAL_WEB_UI_PATH_PREFIX --log=/config/Ripper.log --user=$OPTIONAL_WEB_UI_USERNAME --pass=$OPTIONAL_WEB_UI_PASSWORD
