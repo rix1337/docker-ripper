b04e73d2aaa3e4f80e6bb337c4453d9e403128e1
RiX <rix1337@users.noreply.github.com>
2022-04-30 14:56:44 +0200

    Update install.sh

--- manual-build/install/install.sh
+++ manual-build/install/install.sh
@@ -24,28 +24,20 @@ MAKEMKV_VERSION=$(curl --silent 'https://forum.makemkv.com/forum/viewtopic.php?f
 # FROM, OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER 
 # DEALINGS IN THE SOFTWARE.
 
-
-
 # This disc requires Java runtime (JRE), but none was found. Certain functions will fail, please install Java. See http://www.makemkv.com/bdjava/ for details.
 set -eux
 # update-alternatives: error: error creating symbolic link '/usr/share/man/man1/rmid.1.gz.dpkg-tmp': No such file or directory
 mkdir -p /usr/share/man/man1
 apt-get update
-# FPL_MainFeature detection only seemed to work with OpenJDK 9 (8 was insufficient)
 apt-get install -y --no-install-recommends openjdk-11-jre-headless
 rm -rf /var/lib/apt/lists/*
 
-# beta code: http://www.makemkv.com/forum2/viewtopic.php?f=5&t=1053
 set -ex
 savedAptMark="$(apt-mark showmanual)"
 apt-get update
-apt-get install -y --no-install-recommends ca-certificates g++ gcc gnupg dirmngr libavcodec-dev libexpat-dev libssl-dev make pkg-config qtbase5-dev wget zlib1g-dev	
+apt-get install -y --no-install-recommends ca-certificates g++ gcc gnupg dirmngr libavcodec-dev libexpat-dev libssl-dev make pkg-config qtbase5-dev wget zlib1g-dev
 rm -rf /var/lib/apt/lists/*
-
 wget -O 'sha256sums.txt.sig' "https://www.makemkv.com/download/makemkv-sha-${MAKEMKV_VERSION}.txt"
-# pub   dsa2048 2017-05-26 [SC]
-#       2ECF 2330 5F1F C0B3 2001  6733 94E3 083A 1804 2697
-# uid           [ unknown] MakeMKV (signature) <support@makemkv.com>
 export GNUPGHOME="$(mktemp -d)"
 gpg --batch --keyserver keyserver.ubuntu.com --recv-keys 2ECF23305F1FC0B32001673394E3083A18042697
 gpg --batch --decrypt --output sha256sums.txt sha256sums.txt.sig
@@ -53,7 +45,7 @@ gpgconf --kill all
 rm -rf "$GNUPGHOME" sha256sums.txt.sig
 
 export PREFIX='/usr/local'
-for ball in makemkv-oss makemkv-bin; do 
+for ball in makemkv-oss makemkv-bin; do
 	wget -O "$ball.tgz" "https://www.makemkv.com/download/${ball}-${MAKEMKV_VERSION}.tar.gz"
 	sha256="$(grep "  $ball-${MAKEMKV_VERSION}[.]tar[.]gz\$" sha256sums.txt)"
 	sha256="${sha256%% *}"
